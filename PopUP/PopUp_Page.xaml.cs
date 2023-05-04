b73d404c081dec88f25f9b24cb29cce6047583b9
Grigori Gritsenko <grissha2002@gmail.com>
2023-05-04 12:54:39 +0300

    update

--- PopUP/PopUp_Page.xaml.cs
+++ PopUP/PopUp_Page.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
 using Xamarin.Forms.Xaml;
 
 namespace PopUP
@@ -63,6 +65,8 @@ namespace PopUP
             // Display the question
             var response = await DisplayPromptAsync($"Küsimus {questionIndex + 1}", $"Kui palju on {number1} x {number2}?", placeholder: "Vastus");
 
+            await SaveDataToFileAsync("test.txt", $"{number1} x {number2} = {answer}\n");
+
             // Check the answer
             if (int.TryParse(response, out int userAnswer))
             {
@@ -111,6 +115,40 @@ namespace PopUP
         private async void Button_Clicked(object sender, EventArgs e)
         {
             await Navigation.PopAsync();
+        }
+
+        async Task SaveDataToFileAsync(string fileName, string data)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            string fullPath = Path.Combine(path, fileName);
+
+            using (StreamWriter sw = new StreamWriter(fullPath, true))
+            {
+                await sw.WriteAsync(data).ConfigureAwait(false);
+            }
+        }
+
+        async void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string fullPath = Path.Combine(path, "test.txt");
+
+            if (File.Exists(fullPath))
+            {
+                try
+                {
+                    File.Delete(fullPath);
+                    await DisplayAlert("Korras", $"Faili kustutamine õnnestus", "OK");
+                }
+                catch (Exception ex)
+                {
+                    await DisplayAlert("Viga", $"Viga faili kustutamisel: {ex.Message}", "OK");
+                }
+            }
+            else
+            {
+                await DisplayAlert("Viga", "Faili ei leitud", "OK");
+            }
         }
     }
 }
